ac46430cfac7545d53c79a8787b54de45d98aa47
Ilya Budkov <androidel93@gmail.com>
2022-05-25 21:20:27 +0700

    Добавил функции для отображения задач и фильтрации по дате

--- script.js
+++ script.js
@@ -1,8 +1,66 @@
 var allTasks = null; //Полный список задач
+var currentTasks = null; //Список отображаемых задач
+
+//Функция вычисляет и присваивает timestamp каждой задаче списка tasks
+function setTasksTimestamp(tasks) {
+    for (let i = 0; i < tasks.length; i++) {
+        tasks[i]['timestamp'] = new Date(tasks[i]['date']).getTime();
+    }
+}
+
+//Функция возвращает отфильтрованный список задач на указаный день
+//date - Объект Date, хранящий дату по которой осуществляется отбор задач
+//source - Исходный массив задач
+//return - Отфильтрованый по дате список задач
+function filterTasksByDay(date, source) {
+    let tasks = [];
+    date.setHours(0, 0, 0, 0);
+    let todayFrom = date.getTime();
+    date.setHours(23, 59, 59, 999);
+    let todayTo = date.getTime();
+
+    for (let i = 0; i < source.length; i++) {
+        if ((source[i]['timestamp'] >= todayFrom) && (source[i]['timestamp'] <= todayTo)) {
+            tasks.push(source[i]);
+        }
+    }
+    return tasks;
+}
+
+//Функция отображает информацию для выбранного дня
+//date - объект Date.
+function showByDate(date) {
+    //Сменить текст текущей даты
+    $("#list_date").text(new Intl.DateTimeFormat('ru-RU', {month:'long', day:'numeric', year: 'numeric'}).format(date));
+    //Отфильтровываем список задач на сегодня
+    currentTasks = filterTasksByDay(date, allTasks);
+    //И отображаем их
+    showTasks(currentTasks);
+}
+
+//Отображение на странице задач из списка tasks
+function showTasks(tasks) {
+    $('#list_container').html("");
+    for (let i = 0; i < tasks.length; i++) {
+        let task = $('<div></div>').attr("class","task");
+        task.append($('<div></div>').attr("class","task_name").text(tasks[i]['name']));
+        task.append($('<div></div>').attr("class","task_description").text(tasks[i]['shortDesc']));
+        task.append($('<div></div>').attr("class","task_date").text(tasks[i]['date']));
+        task.append($('<input>').attr("type","checkbox").attr("class","task_check").attr("checked",tasks[i]['status']));
+
+        $('#list_container').append(task);
+    }
+}
 
 $(function() {
     //Установка календаря
-    $("#menu_datepicker").datepicker({});
+    $("#menu_datepicker").datepicker({
+        //При смете даты в календаре
+        onSelect: function(dateText, inst) {
+            //Отображаем задачи на выбранный в календаре день
+            showByDate(new Date(dateText));
+          }
+    });
 
     //У меня не получилось напрямую из JS получить данные с сервера api, пытался долго и безрезультатно.
     //Единственный выход который я нашел, посылать запрос через php, там все оказалось без проблем.
@@ -10,7 +68,7 @@ $(function() {
     //has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
 
     //На скрипт server.php передаю параметром url сервера с которого хочу получить данные
-    //Я решил что удобнее будет получить список всех задач разом и работать с ними здесь на стороне клиента
+    //Я решил что удобнее будет получить список всех задач разом и работать с ними на здесь стороне клиента
     var apiUrl = "https://todo.doczilla.pro/api/todos";
     $.ajax({
         url: "server.php?url=" + encodeURIComponent(apiUrl),
@@ -18,6 +76,10 @@ $(function() {
         success: function(res) {
             //Все полученные задачи будут храниться в переменной allTasks
             allTasks = res;
+            //Для всех задач вычисляю timestamp, мне кажется с ним удобнее и быстрее работать
+            setTasksTimestamp(allTasks);
+            //Отображаем задачи на сегодня
+            showByDate(new Date());
         },
         error: function(res) { alert("Ошибка подключения к серверу!"); }
     });
